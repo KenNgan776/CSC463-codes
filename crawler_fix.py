3bdd4a225b2ab91d06fe046e7ca159c37df2c9ae
KenNgan776 <125227276+KenNgan776@users.noreply.github.com>
2025-12-06 12:59:04 -0500

    Add files via upload

---
+++ crawler_fix.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import ray
+
+ray.init(address='auto')
+
+
+# Function to crawl a website and extract links
+@ray.remote
+def crawl(url):
+    print(f"Crawling: {url}")
+    time.sleep(20)  # Add a delay of 20 seconds so that you can see job running in dashboard
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = set()
+    for a_tag in soup.find_all('a', href=True):
+        link = a_tag['href']
+        if link.startswith('http'):
+            links.add(link)
+    return links
+
+
+# List of URLs to crawl
+urls_to_crawl = [
+    "https://spotify.com", 
+    "https://bofa.com", 
+    "https://google.com", 
+    "https://facebook.com", 
+    "https://amazon.com", 
+    "https://netflix.com", 
+    "https://chase.com", 
+    "https://wikipedia.org", 
+    "https://github.com", 
+    "https://apple.com"
+]
+
+futures = [crawl.remote(url) for url in urls_to_crawl]
+
+# Crawl each URL sequentially
+results = ray.get(futures)
+
+
+# Collect all unique links from the results
+all_links = set()
+for result in results:
+    all_links.update(result)
+
+# Print the final list of all crawled links
+print(f"All crawled links: {all_links}")
+print(f"\nTotal unique links found: {len(all_links)}")
